96a8f2483dd73d983f9b6541f46942dc646c4c1d
Your Name <you@example.com>
2025-10-05 00:43:42 +0530

    limit ut on counter for 0 to 20

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,13 +4,43 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+
+  const [counter, setCounter] = useState(15)
+
+
+  // let counter = 15
   
+  const addValue = () => {
+    
+    // counter = counter + 1
+    
+    if(counter < 20) {
+      setCounter(counter + 1)
+    }
+  console.log("clicked", counter)
+  
+  }
+
+  const removeValue = () => {
+    if(counter > 0) {
+      setCounter(counter - 1)
+    }
+  }
 
   return (
     <>
       <h1>Chai aur react</h1>
-      <h2>Counter <Value></Value></h2>
+      <h2>Counter Value: {counter}</h2>
       
+      
+      <button
+      onClick={addValue}
+      >Add Value {counter}</button>
+      <br />
+      <button
+      onClick={removeValue}
+      >remove value {counter}</button>
+      <p>footer : {counter}</p>
     </>
   )
 }
