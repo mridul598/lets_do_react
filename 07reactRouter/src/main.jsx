7967bd8cc5d6f9168d0eef9470120aa0d1d03204
Your Name <you@example.com>
2025-10-15 23:40:10 +0530

    react_router_commit

---
+++ 07reactRouter/src/main.jsx
@@ -0,0 +1,38 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './Layout.jsx';
+import Home from "./components/Home/Home"
+import About from "./components/About/About"
+import Contact from "./components/Contact/Contact.jsx"
+
+/*
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home/>
+      },
+      {
+        path: "about",
+        element: <About/>
+
+      },
+      {
+        path: "contact",
+        element: <Contact/>
+
+      }
+    ]
+  }
+])*/About
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
